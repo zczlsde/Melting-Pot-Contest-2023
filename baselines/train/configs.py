f47b45e58b47c69727a970b074d1fddde22479f7
Shilong Deng <55789594+zczlsde@users.noreply.github.com>
2023-09-26 15:19:02 +0100

    Update configs.py

--- baselines/train/configs.py
+++ baselines/train/configs.py
@@ -87,7 +87,7 @@ def get_experiment_config(args, default_config):
         "exp_name": args.exp,
         "stopping": {
                     #"timesteps_total": 1000000,
-                    "training_iteration": 1,
+                    "training_iteration": 500, # Change the epochs here 
                     #"episode_reward_mean": 100,
         },
         "num_checkpoints": 5,
